ecd9b38c6937def1a05410a8267c373d3a6cc2be
chougousui <chougousui@gmail.com>
2019-04-29 12:35:08 +0900

    增加一个全排列工具

---
+++ full_array.py
@@ -0,0 +1,16 @@
+COUNT=0
+def perm(n,begin,end):
+    global COUNT
+    if begin>=end:
+        print(n)
+        COUNT +=1
+    else:
+        i=begin
+        for num in range(begin,end):
+            n[num],n[i]=n[i],n[num]
+            perm(n,begin+1,end)
+            n[num],n[i]=n[i],n[num]
+
+n=["m","d","u","s"]
+perm(n,0,len(n))
+print(COUNT)
